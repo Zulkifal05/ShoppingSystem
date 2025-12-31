ab27b62f9dac22fcd194b2fd6cd4387d5b9bd68f
Zulkifal05 <zulkifalali299@gmail.com>
2025-12-31 16:47:24 +0500

    Class for adding items to cart initialized

--- Cart.h
+++ Cart.h
@@ -2,6 +2,13 @@
 #include <iostream>
 using namespace std;
 
+class AddtoCart {  //Base class to avoid ambiguity in multiple inheritance
+public:
+    void addToCart(int itemID) {
+        cout << "\nAdding item " << itemID << " to cart... (Functionality not implemented in this snippet)\n";
+    }
+};
+
 class Cart {
 public:
     void displayCart() {
