7354a1dbf491a27f5ef9f6d566c63d7fba689193
Zulkifal05 <zulkifalali299@gmail.com>
2025-12-31 14:22:57 +0500

    Menu file completed

--- Menu.h
+++ Menu.h
@@ -15,16 +15,21 @@ public:
             cout << "Please select an option from the menu above : ";
             cin >> choice;
 
-            if((choice == "1") || (choice == "2") || (choice == "3") || (choice == "4") || (choice == "5")) {
-                displayCategories(choice);
+            if((choice == "1") || (choice == "2") || (choice == "3") || (choice == "4") || (choice == "5")) {  //If user selects a category and then pass that to displayCategory function
+                if(choice == "1") choice = "c1";
+                else if(choice == "2") choice = "c2";
+                else if(choice == "3") choice = "c3";
+                else if(choice == "4") choice = "c4";
+                else if(choice == "5") choice = "c5";
+                displayCategory(choice);
             }
-            else if((choice == "B") || (choice == "b")) {
+            else if((choice == "B") || (choice == "b")) {  //If user selects to view cart
                 displayCart();
             }
-            else if((choice == "C") || (choice == "c")) {
+            else if((choice == "C") || (choice == "c")) {  //If user selects to view order history
                 displayOrderHistory();
             }
-            else if((choice == "D") || (choice == "d")) {
+            else if((choice == "D") || (choice == "d")) {  //If user selects to logout
                 cout << "\nLogging out... Goodbye!\n";
                 break;
             }
