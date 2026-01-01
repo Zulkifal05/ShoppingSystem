f933fe0dfeb90d7b02b6762df233eaf751d6c5ae
Zulkifal05 <zulkifalali299@gmail.com>
2026-01-01 12:47:01 +0500

    Done till adding items to cart linked list

--- Cart.h
+++ Cart.h
@@ -2,10 +2,31 @@
 #include <iostream>
 using namespace std;
 
-class AddtoCart {  //Base class to avoid ambiguity in multiple inheritance
+struct cartItem {  //Structure to represent an item in the cart
+    string categoryName , itemName;
+    int stockAvailable, price;
+    cartItem *nextCartItem;
+};
+
+cartItem* headCartItem = nullptr; //Global head pointer for Cart linked list
+
+class AddtoCart {  //Class to add items to cart
 public:
-    void addToCart(int itemID) {
-        cout << "\nAdding item " << itemID << " to cart... (Functionality not implemented in this snippet)\n";
+    void addToCart(string categoryName, string itemName, int stockAvailable, int price) {
+        cartItem* newCartItem = new cartItem();
+        newCartItem->categoryName = categoryName;
+        newCartItem->itemName = itemName;
+        newCartItem->stockAvailable = stockAvailable;
+        newCartItem->price = price;
+        newCartItem->nextCartItem = nullptr;
+
+        // Adding the new item to the cart (linked list)
+        if(headCartItem == nullptr) {  //If cart is empty, new item becomes the head
+            headCartItem = newCartItem;
+        } else {  //Else, append to the start of the list
+            newCartItem->nextCartItem = headCartItem;
+            headCartItem = newCartItem;
+        }
     }
 };
 
